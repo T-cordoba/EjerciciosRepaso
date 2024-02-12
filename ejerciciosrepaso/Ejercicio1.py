565901786bb29fb3a29af189551b614fbac39e04
PaulaS06 <menesesgasca@outlook.com>
2024-02-12 16:55:35 -0500

    Add print function

--- ejerciciosrepaso/Ejercicio1.py
+++ ejerciciosrepaso/Ejercicio1.py
@@ -1,1 +1,7 @@
 ## Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla
+
+name = input ("Por favor digite su nombre: ")
+age = input ("Por favor digite su edad: ")
+
+print ("Su nombre es", name, "y tienes", age, "años.")
+
