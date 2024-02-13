cfc65e3f66e10e561b0fb4055f449bd5b06020cc
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 18:22:14 -0500

    Revert "Def function"
    This reverts commit 943b55bd

--- ejerciciosrepaso/Ejercicio3.py
+++ ejerciciosrepaso/Ejercicio3.py
@@ -1,18 +1,4 @@
 ## Crear un programa que genere una lista de números aleatorios y los imprima en pantalla
 
 
-from random import randint
 def random_list (n):
-    count = 0
-    list = []
-    while count < n:
-        random_numer = randint (1,99)
-        list.append (random_numer)
-        count = count + 1
-
-    return list
-
-list_size = input ("De cuántos números quiere la lista?")
-list_result = random_list(int (list_size))
-
-print (list_result)
