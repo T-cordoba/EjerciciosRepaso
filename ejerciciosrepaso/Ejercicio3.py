943b55bd1abc0a6dd9b46c5c77cb5e911c99be29
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 18:07:09 -0500

    Def function

--- ejerciciosrepaso/Ejercicio3.py
+++ ejerciciosrepaso/Ejercicio3.py
@@ -1,2 +1,4 @@
 ## Crear un programa que genere una lista de números aleatorios y los imprima en pantalla
 
+
+def random_list (n):
--- ejerciciosrepaso/Ejercicio7.py
+++ ejerciciosrepaso/Ejercicio7.py
@@ -1,1 +1,20 @@
 ## Escribir un programa que encuentre el número más grande y el más pequeño en una lista dada
+
+def find_numbers(list):
+    splited_list =list.split(",")
+    lowest_number = 999999
+    highest_number = 0
+    for i in splited_list:
+        current_number = int(i)
+        if current_number < lowest_number:
+            lowest_number = int(i)
+
+        if current_number > highest_number:
+            highest_number = int(i)
+
+
+    return ("El número mayor es ", highest_number, " y el número menor es ", lowest_number)
+
+values = input("Ingrese una lista separada por comas")
+result = find_numbers(values)
+print (result)
