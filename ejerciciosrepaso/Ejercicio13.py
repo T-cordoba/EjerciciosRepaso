e0939fd6b8168dc7216f2b296a382a85e9a9def9
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 20:51:23 -0500

    Add variables

--- ejerciciosrepaso/Ejercicio13.py
+++ ejerciciosrepaso/Ejercicio13.py
@@ -1,2 +1,4 @@
 ## Crear un programa que pida al usuario ingresar dos números y calcule su suma, resta, multiplicación y división
 
+numer_1 = input ("Por favor ingrese un número: ")
+numer_2 = input ("Por favor ingrese otro número: ")
