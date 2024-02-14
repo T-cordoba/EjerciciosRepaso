9e4da648784669c9c27c7a8aeaa2e613aac3cd5c
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 21:21:40 -0500

    Add print and result

--- ejerciciosrepaso/Ejercicio13.py
+++ ejerciciosrepaso/Ejercicio13.py
@@ -2,6 +2,8 @@
 
 num_1 = input ("Por favor ingrese un número: ")
 num_2 = input ("Por favor ingrese otro número: ")
+number = float (num_1)
+number2 = float (num_2)
 
 def add (num_1, num_2):
     return num_1 + num_2
@@ -15,3 +17,12 @@ def multiplication (num_1, num_2):
 def division (num_1, num_2):
     return num_1 / num_2
 
+sum = add  (number, number2)
+sub =  substract  (number, number2)
+mul = multiplication  (number, number2)
+div = division  (number, number2)
+
+print ("La suma de los números que usted dió es:", sum)
+print ("La resta de los números que usted dió es:", sub)
+print ("La multiplicación de los números que usted dió es:", mul)
+print ("La división de los números que usted dió es:", div)
