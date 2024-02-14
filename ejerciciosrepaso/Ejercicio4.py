0aed2c4024734f54f713d52360cd1f54b4a0b661
T-cordoba <tommy.cormo@gmail.com>
2024-02-13 21:38:33 -0500

    Defined "even_or_odd"

--- ejerciciosrepaso/Ejercicio4.py
+++ ejerciciosrepaso/Ejercicio4.py
@@ -1,2 +1,7 @@
 ## Escribir un programa que determine si un número dado es par o impar.
 
+def even_or_odd(n):
+    if n%2 is 0:
+        return f"El numero {n} es par"
+    else:
+        return f"El numero {n} es impar"
