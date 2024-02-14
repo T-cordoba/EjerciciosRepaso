bb13ce4199acb27bd19ee37ed42ba0f9fb7f7bf5
T-cordoba <tommy.cormo@gmail.com>
2024-02-13 21:46:52 -0500

    Defined "sum_list" and add result print

--- ejerciciosrepaso/Ejercicio6.py
+++ ejerciciosrepaso/Ejercicio6.py
@@ -1,1 +1,12 @@
 ## Crear un programa que calcule la suma de los números en una lista dada
+
+def sum_list(l):
+    result=0
+    for i in range(len(l)):
+        result+=l[i]
+    return result
+
+l=[1,2,3,4,5,6]
+
+result=sum_list(l)
+print(f"La suma de los números en la lista {l} es {result}")
