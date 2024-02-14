472f732af8f1329b4376037fea09d4f171fc1497
T-cordoba <tommy.cormo@gmail.com>
2024-02-13 21:33:01 -0500

    Add "ask_radius" and "area_result"

--- ejerciciosrepaso/Ejercicio2.py
+++ ejerciciosrepaso/Ejercicio2.py
@@ -1,5 +1,9 @@
 ## Escribir una función que calcule el área de un círculo dado su radio.
 
 import math
+
 def circle_area (r):
     return math.pi*r**2
+
+ask_radius = int(input("Ingrese el radio del circulo "))
+area_result = circle_area(ask_radius)
