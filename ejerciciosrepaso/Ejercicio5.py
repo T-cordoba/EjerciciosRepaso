57ef24e1e00c5960cac9def60e8d2c20895fd4f6
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 19:59:29 -0500

    Add instruction 5

--- ejerciciosrepaso/Ejercicio5.py
+++ ejerciciosrepaso/Ejercicio5.py
@@ -1,14 +1,1 @@
 ## Crear una función que convierta grados Fahrenheit a grados Celsius
-
-def fahrenheit_to_celsius (fahrenheit):
-
-    fahrenheit = float (fahrenheit)
-
-    celsius = (fahrenheit - 32) / 1,8
-
-    return celsius
-
-temperature = input ("Ingrese la temperatura en Fahrenheit que va a convertir a Celsius")
-result = fahrenheit_to_celsius (temperature)
-print (temperature, "fahrenheit en grados celsius es:", result)
-
