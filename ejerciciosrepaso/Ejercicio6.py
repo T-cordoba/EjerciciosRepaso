a73a65aebada60ceb79c881701fa52e8ee49a415
T-cordoba <tommy.cormo@gmail.com>
2024-02-13 21:47:15 -0500

    Syntax correction

--- ejerciciosrepaso/Ejercicio6.py
+++ ejerciciosrepaso/Ejercicio6.py
@@ -1,12 +1,11 @@
 ## Crear un programa que calcule la suma de los números en una lista dada
 
+l=[1,2,3,4,5,6]
 def sum_list(l):
     result=0
     for i in range(len(l)):
         result+=l[i]
     return result
-
-l=[1,2,3,4,5,6]
 
 result=sum_list(l)
 print(f"La suma de los números en la lista {l} es {result}")
