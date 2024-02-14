1b8c00835c70dd58096d1efe2788daddc3c42ea6
T-cordoba <tommy.cormo@gmail.com>
2024-02-14 12:14:52 -0500

    Fixed gen_matrix logic and add print result

--- ejerciciosrepaso/Ejercicio9.py
+++ ejerciciosrepaso/Ejercicio9.py
@@ -2,7 +2,19 @@
 
 import random
 
+m=[
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+    ]
+
 def gen_matrix(m):
-    m=[]
-    for i in range(10):
-        for j in range(10):
+    for i in range(5):
+        for j in range(5):
+            m[i][j]=random.randint(0,20)
+    return m
+
+result=gen_matrix(m)
+print(f"{result[0]}\n{result[1]}\n{result[2]}")
