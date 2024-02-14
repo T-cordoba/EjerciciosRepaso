9c868041128757793ef685f8b334aa2fac838d09
T-cordoba <tommy.cormo@gmail.com>
2024-02-13 22:21:34 -0500

    Defined "arith_average" and tested result

--- ejerciciosrepaso/Ejercicio14.py
+++ ejerciciosrepaso/Ejercicio14.py
@@ -1,3 +1,13 @@
 ## Escribir una función que calcule la media aritmética de una lista de números.
 
 l=[5,1,3,7,2,5]
+
+def arith_average(l):
+    s=0
+    for i in range(len(l)):
+        s+=l[i]
+    return s/len(l)
+
+result=arith_average(l)
+
+print(result)
