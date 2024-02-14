a73bc97da3ef08a194e874de41bac39964ff4eb7
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 20:47:05 -0500

    Add while function Add print function

--- ejerciciosrepaso/Ejercicio11.py
+++ ejerciciosrepaso/Ejercicio11.py
@@ -1,4 +1,13 @@
 ##Crear un programa que genere una lista de números pares entre 1 y 100
 
+list = [ ]
+
 x = 1
 
+while x <= 100:
+    if x % 2 == 0:
+        list.append (x)
+    x = x + 1
+
+
+print ("La lista de los números pares del 1 al 100 es", list)
