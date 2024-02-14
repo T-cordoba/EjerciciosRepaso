f2e45c16cd97fe301362a88616041a6eb1f05601
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 20:29:47 -0500

    Syntax correction

--- ejerciciosrepaso/Ejercicio 7.py
+++ ejerciciosrepaso/Ejercicio 7.py
@@ -1,7 +1,7 @@
 ## Escribir un programa que encuentre el número más grande y el más pequeño en una lista dada
 
 def find_number (list):
-    splited_list = list.split ("")
+    splited_list = list.split (",")
     lowest_number = 999999
     highest_number = 0
 
