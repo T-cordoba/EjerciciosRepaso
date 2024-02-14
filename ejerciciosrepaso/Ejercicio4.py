6f557df61e010f60315e30d5bbb2ff5fdb08e16a
T-cordoba <tommy.cormo@gmail.com>
2024-02-13 21:40:48 -0500

    Syntax correction

--- ejerciciosrepaso/Ejercicio4.py
+++ ejerciciosrepaso/Ejercicio4.py
@@ -1,7 +1,12 @@
 ## Escribir un programa que determine si un número dado es par o impar.
 
 def even_or_odd(n):
-    if n%2 is 0:
+    if n%2 == 0:
         return f"El numero {n} es par"
     else:
         return f"El numero {n} es impar"
+
+num=int(input("Ingrese un número"))
+result=even_or_odd(num)
+
+print(result)
