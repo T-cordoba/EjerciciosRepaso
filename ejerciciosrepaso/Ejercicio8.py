68499b6c073629440b06bee92349f378d581814d
T-cordoba <tommy.cormo@gmail.com>
2024-02-13 22:02:26 -0500

    Fixed syntax error and add print result

--- ejerciciosrepaso/Ejercicio8.py
+++ ejerciciosrepaso/Ejercicio8.py
@@ -1,8 +1,12 @@
 ## Crear una función que invierta el orden de los elementos en una lista dada.
 
-l=[1,2,3,4,5,6]
+l=[1,2,3,4,9,6]
 
 def inv_list(l):
     result=[]
-    for i in range(len(l),0):
+    for i in range((len(l)-1),-1,-1):
         result.append(l[i])
+    return result
+
+result=inv_list(l)
+print(result)
