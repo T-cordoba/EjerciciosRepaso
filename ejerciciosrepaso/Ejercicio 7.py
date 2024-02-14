531cbd442e8e093aba49580d9ce2a2f152bc0f8d
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 20:20:38 -0500

    define and add instruction9

---
+++ ejerciciosrepaso/Ejercicio 7.py
@@ -0,0 +1,6 @@
+## Escribir un programa que encuentre el número más grande y el más pequeño en una lista dada
+
+def find_number (list):
+    splited_list = list.split ("")
+    lowest number = 999999
+    highest_number = 0
