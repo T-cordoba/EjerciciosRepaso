555f05e4d756719398ac861a6dedc430d8188ee3
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 20:53:45 -0500

    Add add function

--- ejerciciosrepaso/Ejercicio13.py
+++ ejerciciosrepaso/Ejercicio13.py
@@ -1,4 +1,8 @@
 ## Crear un programa que pida al usuario ingresar dos números y calcule su suma, resta, multiplicación y división
 
-numer_1 = input ("Por favor ingrese un número: ")
-numer_2 = input ("Por favor ingrese otro número: ")
+num_1 = input ("Por favor ingrese un número: ")
+num_2 = input ("Por favor ingrese otro número: ")
+
+def add (num_1, num_2):
+    return num_1 + num_2
+
