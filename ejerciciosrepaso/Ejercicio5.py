302c3926d1547f862a791811d3c9908a4cab2596
PaulaS06 <menesesgasca@outlook.com>
2024-02-13 20:05:36 -0500

    Add input, result and print

--- ejerciciosrepaso/Ejercicio5.py
+++ ejerciciosrepaso/Ejercicio5.py
@@ -6,3 +6,7 @@ def fahrenheit_to_celsius (fahrenheit):
     celsius = (fahrenheit - 32) / 1, 8
 
     return celsius
+
+temperature = input ("Ingrese la temperatura en Fahrenheit que va a convertir a Celsius")
+result = fahrenheit_to_celsius (temperature)
+print (temperature, "fahrenheit en grados celsius es:", result)
